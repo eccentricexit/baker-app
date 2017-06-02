1c198edde087336bc068940f8a9365c560b21554
Matheus Faria <mtsalenc@gmail.com>
2017-06-01 22:26:10 -0300

    fix: Add missing import

--- app/src/androidTest/java/com/deltabit/bakingapp/BrownieRecipeContentTest.java
+++ app/src/androidTest/java/com/deltabit/bakingapp/BrownieRecipeContentTest.java
@@ -24,6 +24,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.deltabit.bakingapp.AppNavigationTest.withRecyclerView;
 import static org.hamcrest.Matchers.allOf;
